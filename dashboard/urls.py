4c9daa91f245a8e9f4fe41e19113836f41b10d9f
Edouard <edouardtuyubahe@gmail.com>
2026-08-26 02:55:33 +0200

    fix: add router basenames and agent model tests

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,8 @@ from rest_framework.routers import DefaultRouter
 from .views import EventViewSet, AgentTaskViewSet, ApprovalRequestViewSet
 
 router = DefaultRouter()
-router.register('events', EventViewSet)
-router.register('tasks', AgentTaskViewSet)
-router.register('approvals', ApprovalRequestViewSet)
+router.register('events', EventViewSet, basename='event')
+router.register('tasks', AgentTaskViewSet, basename='agenttask')
+router.register('approvals', ApprovalRequestViewSet, basename='approvalrequest')
 
 urlpatterns = router.urls
